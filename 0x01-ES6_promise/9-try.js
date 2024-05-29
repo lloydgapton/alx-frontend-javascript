670a41451265e8bde47d63229515d75ba398aa89
lloydgapton <lloydgapton123@gmail.com>
2024-05-29 14:54:44 -0700

    updated prject files

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,9 +1,13 @@
-export default function guardrail(mathFunction) {
-    const queue = [];
-    try {
-      queue.push(mathFunction(), 'Guardrail was processed');
-    } catch (err) {
-      queue.push(`Error: ${err.message}`, 'Guardrail was processed');
-    }
-    return queue;
+export default function mathFunction(guardrail) {
+  const queue = [];
+  try {
+    const x = guardrail();
+    queue.push(x);
+  } catch (err) {
+    queue.push(`${err.name}: ${err.message}`);
+  } finally {
+    queue.push('Guardrail was processed');
   }
+
+  return queue;
+}
