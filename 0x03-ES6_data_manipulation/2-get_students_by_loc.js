d40b081f43ea8668233717c150240d7a01ffb921
lloydgapton <lloydgapton123@gmail.com>
2024-06-04 15:25:56 -0700

    updated file

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,7 +1,4 @@
-export default function getStudentsByLocation(students, city) {
-    // check arg is an array before using filter
-    if (Object.getPrototypeOf(students) === Array.prototype) {
-      return students.filter((items) => items.location === city);
-    }
-    return [];
-  }
+export default function getStudentsByLocation(studentList, city) {
+  const newArray = studentList.filter((student) => student.location === city);
+  return newArray;
+}}
