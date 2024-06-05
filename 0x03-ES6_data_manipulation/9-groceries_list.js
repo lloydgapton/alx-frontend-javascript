7837e3cd99f7d8c6eef9b0831e15c598ba1e5aa3
lloydgapton <lloydgapton123@gmail.com>
2024-06-04 17:20:30 -0700

    updated file

--- 0x03-ES6_data_manipulation/9-groceries_list.js
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -1,9 +1,10 @@
 export default function groceriesList() {
-    const groceries = new Map();
-    groceries.set('Apples', 10);
-    groceries.set('Tomatoes', 10);
-    groceries.set('Pasta', 1);
-    groceries.set('Rice', 1);
-    groceries.set('Banana', 5);
-    return groceries;
-  }
+  const map1 = new Map();
+  map1.set('Apples', 10);
+  map1.set('Tomatoes', 10);
+  map1.set('Pasta', 1);
+  map1.set('Rice', 1);
+  map1.set('Banana', 5);
+
+  return map1;
+}
